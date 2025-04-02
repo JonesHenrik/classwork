187313416083ddccbb6bb9d5d3e7f8faa4cc9797
Henrik Jones <henrikjones22@gmail.com>
2025-04-02 15:14:13 -0400

    base logic implemented

--- Unit-2-Intro-To-C#/NumberAnalyzerLab/NumberAnalyzerLab/Program.cs
+++ Unit-2-Intro-To-C#/NumberAnalyzerLab/NumberAnalyzerLab/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var isPlaying = true;
+        while (isPlaying)
+        {
+            Console.WriteLine("Please enter a number from 1 to 100: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number >= 1 && number <= 100)
+            {
+                // % is the Modolus operator and basically displays the remainder
+            }
+            else
+            { 
+                Console.WriteLine("You must enter a number from 1 to 100: ");
+            }
+        }
     }
 }
