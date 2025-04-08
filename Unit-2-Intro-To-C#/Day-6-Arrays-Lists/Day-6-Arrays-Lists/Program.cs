7c636850595c7e5b67421ae41feb1ca48ae94610
Henrik Jones <henrikjones22@gmail.com>
2025-04-07 22:02:25 -0400

    Intro to loops

--- Unit-2-Intro-To-C#/Day-6-Arrays-Lists/Day-6-Arrays-Lists/Program.cs
+++ Unit-2-Intro-To-C#/Day-6-Arrays-Lists/Day-6-Arrays-Lists/Program.cs
@@ -7,36 +7,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Define the variables to hold the numbers we need to add
-        int number1 = 0; // Since we will be doing math it needs to be numeric
-        int number2 = 0; // Since we will be doing math it needs to be numeric
-        int number3 = 0; // Since we will be doing math it needs to be numeric
+        // Define an array to hold 3 numbers
+        int[] numbers = new int[5]; 
+        
+        
 
         // Define a variable to hold the sum of the numbers
         int sum = 0;
 
         Console.WriteLine("--- Starting program ---");
 
-        // Ask for the numbers one at time using the C# Console object
-        //     which represents the keyboard and screen
-        // Console.ReadLine() returns a string from keyboard - cannot store in an int
-        Console.WriteLine("Please enter a number:  "); // Asking for the number
-        string theResponse; // Define a string to hold the line of input from the keyboard
-        theResponse = Console.ReadLine(); // Get a line from the keyboard
-      
-        // int.Parse(string) will convert a string to an int
-        number1 = int.Parse(theResponse);
-
-        Console.WriteLine("Please enter a number:  ");
-        theResponse = Console.ReadLine(); // Reuse theResponse define above
-        number2 = int.Parse(theResponse);
-
-        Console.WriteLine("Please enter a number:  ");
-        theResponse = Console.ReadLine(); // Reuse theResponse define above
-        number3 = int.Parse(theResponse);
-
-        // Add the numbers together create a sum/total
-        sum = number1 + number2 + number3;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine("Enter a number: ");
+            numbers[i] = int.Parse(Console.ReadLine());
+            
+            sum += numbers[i];
+           
+        }
        
         // Tell the requester the sum/total (display)
         Console.WriteLine("The sum is: " + sum);
