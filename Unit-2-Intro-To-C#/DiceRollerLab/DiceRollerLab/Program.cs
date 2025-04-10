681098ae629d5ac6c3e9435e1aa92796d48ff5c9
Henrik Jones <henrikjones22@gmail.com>
2025-04-10 19:53:34 -0400

    playing with DiceRollerLab

---
+++ Unit-2-Intro-To-C#/DiceRollerLab/DiceRollerLab/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DiceRollerLab;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter the amount of sides on your dice you would like");
+        Console.WriteLine("Your dice will have a max value of: " + maxDieSelection());
+    }
+
+    static int maxDieSelection()
+    {
+        int sides = 0;
+        bool correctInput = false;
+        while (!correctInput)
+        {
+            sides = int.Parse(Console.ReadLine());
+            if (sides >= 0 && sides <= 6)
+            {
+                correctInput = true;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number between 0 and 6");
+            }
+        }
+        return sides;
+        
+    }
+
+    static int rollDice(int sides)
+    {
+        
+    }
+}
