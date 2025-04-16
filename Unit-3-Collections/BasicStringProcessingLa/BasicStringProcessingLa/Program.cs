cc13a989180034d216764d0c6ed9e2fc096edbec
Henrik Jones <henrikjones22@gmail.com>
2025-04-16 15:42:12 -0400

    completed lab

--- Unit-3-Collections/BasicStringProcessingLa/BasicStringProcessingLa/Program.cs
+++ Unit-3-Collections/BasicStringProcessingLa/BasicStringProcessingLa/Program.cs
@@ -4,6 +4,74 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        firstExample();
+        secondExample();
+    }
+    static void firstExample()
+    {
+        bool isEnteringSentences = false;
+        do
+        {
+            Console.WriteLine("Enter a sentence: ");
+            string input = Console.ReadLine();
+
+            char[] arrayOfInput = new char[] { ' ' };
+
+            string[] output = input.Split(arrayOfInput);
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                Console.WriteLine(output[i]);
+            }
+
+            Console.WriteLine("Would you like to continue? (y/n)? ");
+            string continueResponse = Console.ReadLine();
+            string firstChar = continueResponse.Substring(0, 1).ToLower();
+            
+            if (continueResponse == "y")
+            {
+                isEnteringSentences = true;
+            }
+            else
+            {
+                isEnteringSentences = false;
+                Console.WriteLine("Goodbye!");
+            }
+
+        } while (isEnteringSentences);
+    }
+
+    static void secondExample()
+    {
+        bool isEnteringStrings = false;
+        List<String> listOfInputs = new List<String>();
+        
+        do
+        {
+            Console.WriteLine("Enter some text: ");
+            string input = Console.ReadLine();
+            
+            listOfInputs.Add(input);
+            string sentence = "";
+            
+            for (int i = 0; i < listOfInputs.Count; i++) 
+            {
+                sentence += listOfInputs[i] + " ";
+                Console.WriteLine("You have entered: " + sentence);
+            }
+            
+            Console.WriteLine("Would you like to continue? (y/n)? ");
+            string continueResponse = Console.ReadLine();
+            continueResponse = continueResponse.Substring(0, 1).ToLower();
+
+            if (continueResponse == "y")
+            {
+                isEnteringStrings = true;
+            } else if (continueResponse == "n")
+            {
+                isEnteringStrings = false;
+                Console.WriteLine("Goodbye!");
+            }
+        } while (isEnteringStrings);
     }
 }
