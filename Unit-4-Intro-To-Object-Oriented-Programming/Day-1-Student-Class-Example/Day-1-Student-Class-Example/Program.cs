28c40ddef5f1e1d05ce5ffb145c2a50d68791f1f
Henrik Jones <henrikjones22@gmail.com>
2025-04-23 18:34:58 -0400

    finishing last class

---
+++ Unit-4-Intro-To-Object-Oriented-Programming/Day-1-Student-Class-Example/Day-1-Student-Class-Example/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Day_1_Student_Class_Example;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Unit-4-Intro-To-Object-Oriented-Programming/Day-1-Student-Class-Example/Day-1-Student-Class-Example/Student.cs
@@ -0,0 +1,13 @@
+namespace Day_1_Student_Class_Example;
+
+public class Student
+{
+    // Private implements Object-Oriented principle of Encapsulation
+    // Encapsulation - class should protect data from outside access
+    //                  only methods in the class can access the data 
+    
+    private string studentName;
+    private List<int> testScores;
+    
+    
+}
