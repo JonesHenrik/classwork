6ec5f24cc3b06a9b873667fb57bae5670b00c564
Henrik Jones <henrikjones22@gmail.com>
2025-05-06 15:55:00 -0400

    first two steps completed

---
+++ Unit-4-Intro-To-Object-Oriented-Programming/MovieLab/MovieLab/Movie.cs
@@ -0,0 +1,13 @@
+namespace MovieLab;
+
+public class Movie
+{
+    string title;
+    string category;
+
+    public Movie(string title, string category)
+    {
+        title = title;
+        category = category;
+    }
+}
--- Unit-4-Intro-To-Object-Oriented-Programming/MovieLab/MovieLab/Program.cs
+++ Unit-4-Intro-To-Object-Oriented-Programming/MovieLab/MovieLab/Program.cs
@@ -2,8 +2,27 @@
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to the Movie List Application!\n");
+    
+        // Create list of Movies class
+        //private List<Movie> listOfMovies = [Movie("John Wick", "Action")];
+        // Add at least ten Movies to the list
+        // Print amount of movies in the list to the Console
+        List<Movie> movies = new List<Movie>
+        {
+            new Movie("John Wick", "Action"),
+            new Movie("Gladiator", "Action"),
+            new Movie("The Dark Knight", "Action"),
+            new Movie("Superbad", "Comedy"),
+            new Movie("The Grand Budapest Hotel", "Comedy"),
+            new Movie("Hereditary", "Horror"),
+            new Movie("The Conjuring", "Horror"),
+            new Movie("Get Out", "Horror"),
+            new Movie("Inception", "Science Fiction"),
+            new Movie("Intersteller", "Science Fiction"),
+        };
     }
 }
