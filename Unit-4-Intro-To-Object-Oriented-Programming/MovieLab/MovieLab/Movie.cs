29105943f2d239805fc469738fff58605c38f294
Henrik Jones <henrikjones22@gmail.com>
2025-05-07 19:54:02 -0400

    added get properties to Movie class

--- Unit-4-Intro-To-Object-Oriented-Programming/MovieLab/MovieLab/Movie.cs
+++ Unit-4-Intro-To-Object-Oriented-Programming/MovieLab/MovieLab/Movie.cs
@@ -5,6 +5,17 @@ public class Movie
     string title;
     string category;
 
+
+    public string Title
+    {
+        get { return title; }
+    }
+
+    public string Category
+    {
+        get { return category; }
+    }
+    
     public Movie(string title, string category)
     {
         title = title;
