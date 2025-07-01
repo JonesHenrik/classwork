067a5bcb42a7f9a5774c150761c4fbf0a701ef02
Henrik Jones <henrikjones22@gmail.com>
2025-06-30 22:00:52 -0400

    cuilding out my own components

--- Unit-7-Angular/Day-4-Multi-Page-App/src/app/app.routes.ts
+++ Unit-7-Angular/Day-4-Multi-Page-App/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes }        from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { StateSealsComponent } from './components/state-seals/state-seals.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
 
+
+// The Routes object is an array of objects to associate a URL path to a component
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent},
+    {path: 'seals', component: StateSealsComponent},
+    {path: 'student-list', component: StudentListComponent}
 ];
--- Unit-7-Angular/Day-4-Multi-Page-App/src/app/components/nav-bar/nav-bar.component.ts
+++ Unit-7-Angular/Day-4-Multi-Page-App/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { StateSealsComponent } from '../state-seals/state-seals.component';
 
 @Component({
-  selector: 'nav-bar',
+  selector: 'nav-bar', // is used in the html to include this component
   standalone: true,
   imports: [],
-  templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  templateUrl: './nav-bar.component.html', // is the file with the html for the component
+  styleUrl: './nav-bar.component.css' // file with the css for the component
 })
 export class NavBarComponent {
-
+  
 }
